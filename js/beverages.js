74bfd553fdb8749830ccdc3b18d6bb66d63fb023
JFarrell007 <jimmyfarrell007@gmail.com>
2018-12-06 16:23:36 -0500

    update template

--- js/beverages.js
+++ js/beverages.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     Mustache.parse(template);
 
     var rendered = Mustache.render(template, {
-        drink: {
-            category: "Wine"
+        drink1: {
+            category: "White Wine"
         },
         white: [
             {
@@ -33,11 +33,81 @@ $(document).ready(function(){
                 price: "8"
             },
             {
-                name: "Dewey and Finn's 2018 Canine Classic",
+                name: "Dewey and Finn's 2018 Canine White Classic",
                 location: "Georgia",
                 price: "10"
             }
         ],
+        drink2: {
+            category: "Red Wine"
+        },
+        red: [
+            {
+                name: "Vetana 2013 Pinot Noir",
+                location: "California, Monterey County, Arroyo Seco",
+                price: "12"
+            },
+            {
+                name: "Frei Brothers Reserve 2016 Merlot",
+                location: "California, Dry Creek Valley",
+                price: "13"
+            },
+            {
+                name: "Rascal 2017 Pinot Noir",
+                location: "Willamette Vally, Oregon",
+                price: "8"
+            },
+            {
+                name: "Luna Nuda 2016 Red Blend",
+                location: "Italy, Alto Adige",
+                price: "10"
+            },
+            {
+                name: "Amuse Bouche 2012 Red Bordeax Blend",
+                location: "California, Napa Valley",
+                price: "53"
+            },
+            {
+                name: "Dewey and Finn's 2018 Canine Red Classic",
+                location: "Georgia",
+                price: "11"
+            }
+        ],  
+        drink3: {
+            category: "Beer on tap"
+        },
+        beer: [
+            {
+                name: "Blue Moon 5.4%",
+                location: "Golden, CO",
+                price: "6"
+            },
+            {
+                name: "Monday Night Fu Man Brew 5.2%",
+                location: "Atlanta, GA",
+                price: "7"
+            },
+            {
+                name: "BELL'S Amber 5.8%",
+                location: "Kalamazoo, MI",
+                price: "8"
+            },
+            {
+                name: "Spaten Oktoberfest 5.9%",
+                location: "Germany",
+                price: "8"
+            },
+            {
+                name: "Kentucky Bourbon Barrel 8.2%",
+                location: "Lexington, KY",
+                price: "8"
+            },
+            {
+                name: "Dewey and Finn's Big Dog Ale",
+                location: "Georgia",
+                price: "7"
+            }
+        ],              
     })
     $("#target").html(rendered);
 });
